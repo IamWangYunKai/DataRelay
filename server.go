c974654048d1f8a048ae0cde35dc767ddea55cf1
IamWangYunKai <615349118@163.com>
2020-05-29 22:18:06 +0800

    add TCP, have muti-connection bug

--- server.go
+++ server.go
@@ -7,12 +7,16 @@ import (
 )
 
 var portsDict = map[string]int{
-    "vision":10001,
+    //"vision":10001,
     "sensor":10002,
     "cmd":10003,
     "debug":10004,
     "clock":10005,
     "message":10006,
+}
+
+var portsDictTCP = map[string]int{
+	"vision":10001,
 }
 
 type Message struct {
@@ -24,8 +28,11 @@ type Message struct {
 
 var clientIPs = make(map[string]string)
 var clientPorts = make(map[string]int)
-
 var clientSockets = make(map[string]*net.UDPConn)
+
+var clientTCPIPs = make(map[string]string)
+var clientTCPPorts = make(map[string]int)
+var clientTCPSockets = make(map[string]*net.Conn)
 
 var done = make(chan struct{})
 
@@ -33,17 +40,11 @@ func read(socket *net.UDPConn, key string) {
 	for {
 		data := make([]byte, 65535)
 		n, remoteAddr, err := socket.ReadFromUDP(data)
-		if err != nil {
-			fmt.Printf("error during read: %s", err)
-			continue
-		}
+		checkErr(err)
 		// fmt.Printf("receive %s from <%s>\n", data[:n], remoteAddr)
 		ip, _port, err := net.SplitHostPort(remoteAddr.String())
 		port,err := strconv.Atoi(_port)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+		checkErr(err)
 		var message Message
 		if err := json.Unmarshal(data[:n], &message); err != nil {
 			go relayNoId(data, n, port, key)
@@ -51,7 +52,7 @@ func read(socket *net.UDPConn, key string) {
 			continue
 		}
 		if message.Mtype == "register" {
-			fmt.Printf("Register <%s:%s> %s\n", key, message.Id, message.Data)
+			//fmt.Printf("Register <%s:%s> %s\n", key, message.Id, message.Data)
 			clientIPs[message.Id] = ip
 			clientPorts[message.Id+":"+key] = port
 			feedback := Message{
@@ -61,10 +62,7 @@ func read(socket *net.UDPConn, key string) {
 				Data: remoteAddr.String(),
 			}
 			feedbackStr, err := json.Marshal(feedback)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
+			checkErr(err)
 			socket.WriteToUDP(feedbackStr, remoteAddr)
 		} else {
 			go relay(data, n, message.Id, key)
@@ -96,17 +94,95 @@ func relayNoId(data []byte, n int, port int, key string) {
 	}
 }
 
+func checkErr(err error){
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func relayTCP(data []byte, n int, robotID string, key string) {
+	for otherID, _ := range clientTCPIPs{
+		if otherID == robotID {
+			continue
+		}
+		if _, ok := clientTCPPorts[otherID+":"+key]; ok {
+			(*clientTCPSockets[key]).Write(data[:n])
+		}
+	}
+}
+
+func relayTCPRaw(data []byte, n int, port int, key string) {
+	fmt.Println("relayTCPRaw", n, port, key)
+	for otherID, _ := range clientTCPIPs{
+		if _, ok := clientTCPPorts[otherID+":"+key]; ok {
+			if clientTCPPorts[otherID+":"+key] == port {
+				continue
+			}
+			if _, ok := clientTCPPorts[otherID+":"+key]; ok {
+				(*clientTCPSockets[key]).Write(data[:n])
+				fmt.Println("write to", otherID, key)
+			}
+		}
+	}
+}
+
+func readTCP(socket *net.TCPListener, key string){
+	for {
+		conn, err := socket.Accept()
+		fmt.Println("accept tcp client",conn.RemoteAddr().String())
+		checkErr(err)
+		clientTCPSockets[key] = &conn
+		remoteAddr := conn.RemoteAddr()
+		ip, _port, err := net.SplitHostPort(remoteAddr.String())
+		port,err := strconv.Atoi(_port)
+		checkErr(err)
+		for {
+			data := make([]byte, 65535*5)
+			n, err := conn.Read(data)
+			fmt.Println("Get data", n)
+			var message Message
+			if err := json.Unmarshal(data[:n], &message); err != nil {
+				go relayTCPRaw(data, n, port, key)
+			}
+			checkErr(err)
+			if message.Mtype == "register" {
+				fmt.Printf("Register <%s:%s> %s\n", key, message.Id, message.Data)
+				clientIPs[message.Id] = ip
+				clientPorts[message.Id+":"+key] = port
+				feedback := Message{
+					Mtype: "register",
+					Pri:   5,
+					Id:    "000000",
+					Data:  remoteAddr.String(),
+				}
+				feedbackStr, err := json.Marshal(feedback)
+				checkErr(err)
+				_, err = conn.Write(feedbackStr)
+				checkErr(err)
+			} else {
+				go relayTCP(data, n, message.Id, key)
+			}
+		}
+	}
+}
+
 func main() {
 	for key, port := range portsDict {
 		clientAddr := &net.UDPAddr{IP: net.IPv4zero, Port: port}
 		clientListener, err := net.ListenUDP("udp", clientAddr)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+		checkErr(err)
 
 		clientSockets[key] = clientListener
 		go read(clientListener, key)
+	}
+
+	for key, port := range portsDictTCP {
+		clientAddr, err := net.ResolveTCPAddr("tcp4", ":"+strconv.Itoa(port))
+		checkErr(err)
+		clientListener, err := net.ListenTCP("tcp", clientAddr)
+		checkErr(err)
+		fmt.Println("Start to read TCP")
+		go readTCP(clientListener, key)
 	}
 	fmt.Println("Start to work ...")
 	<-done
