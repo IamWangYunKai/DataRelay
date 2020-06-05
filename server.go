e31e10d08da3bab62a75c60a5c0d61c287ede1aa
IamWangYunKai <615349118@163.com>
2020-06-05 23:17:31 +0800

    add web server visualization

--- server.go
+++ server.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"net/http"
 	"strconv"
 	"sync"
-	//"./syncmap"
+	"text/template"
+	"time"
 )
 
 var portsDict = map[string]int{
-    //"vision":10001,
     "sensor":10002,
     "cmd":10003,
     "debug":10004,
@@ -22,20 +23,43 @@ var portsDict = map[string]int{
 var portsDictTCP = map[string]int{
 	"vision":10001,
 }
-
+// Comunication protocol
 type Message struct {
   Mtype string
   Pri int
   Id string
   Data string
 }
-
+// Thread-safe maps
 var clientUDPIPs, clientUDPPorts, clientUDPSockets sync.Map
 var clientTCPIPs, clientTCPPorts, clientTCPSockets sync.Map
-
+var UDPRecvCnt, TCPRecvCnt, UDPRelayCnt, TCPRelayCnt sync.Map
+var UDPStateMap, TCPStateMap sync.Map
+// Block the main function
 var done = make(chan struct{})
 
-func read(socket *net.UDPConn, key string) {
+// Web visualization information
+type SocketState struct {
+	Key string
+	Type string
+	IP string
+	Port int
+	RecvFPS int
+	RelayFPS int
+}
+
+// index.html resource
+var myTemplate *template.Template
+var mutex sync.RWMutex
+var socketStates []SocketState
+
+func checkErr(err error){
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func readUDP(socket *net.UDPConn, key string) {
 	for {
 		data := make([]byte, 65535)
 		n, remoteAddr, err := socket.ReadFromUDP(data)
@@ -44,9 +68,9 @@ func read(socket *net.UDPConn, key string) {
 		port,err := strconv.Atoi(_port)
 		checkErr(err)
 		var message Message
-		if err := json.Unmarshal(data[:n], &message); err != nil {
-			go relayUDPRaw(data, n, port, key)
-			//fmt.Println(err)
+		err = json.Unmarshal(data[:n], &message)
+		if err != nil || message.Id == ""{
+			go relayUDPRaw(data, n, ip, port, key)
 			continue
 		}
 		if message.Mtype == "register" {
@@ -61,15 +85,22 @@ func read(socket *net.UDPConn, key string) {
 			}
 			feedbackStr, err := json.Marshal(feedback)
 			checkErr(err)
+
+			SocketState := SocketState{Key:key, Type:"UDP", IP: ip, Port: port, RecvFPS:0, RelayFPS:0}
+			UDPStateMap.Store(message.Id+":"+key, SocketState)
+			UDPRecvCnt.Store(message.Id+":"+key, 0)
+			UDPRelayCnt.Store(message.Id+":"+key, 0)
 			socket.WriteToUDP(feedbackStr, remoteAddr)
 		} else {
-			go relay(data, n, message.Id, key)
+			go relayUDP(data, n, message.Id, key)
 		}
 	}
 }
 
-func relay(data []byte, n int, myID string, key string) {
-	clientUDPIPs.Range(func(robotID,ip interface{})bool{
+func relayUDP(data []byte, n int, myID string, key string) {
+	cnt, _ := UDPRecvCnt.Load(myID+":"+key)
+	UDPRecvCnt.Store(myID+":"+key, cnt.(int)+1)
+	clientUDPIPs.Range(func(robotID, ip interface{})bool{
 		if robotID.(string) == myID {
 			return true
 		}
@@ -84,19 +115,43 @@ func relay(data []byte, n int, myID string, key string) {
 			return true
 		}
 		socket.(*net.UDPConn).WriteToUDP(data[:n], clientAddr)
+		cnt, _ := UDPRelayCnt.Load(myID+":"+key)
+		UDPRelayCnt.Store(myID+":"+key, cnt.(int)+1)
 		return true
 	})
 }
 
-func relayUDPRaw(data []byte, n int, myPort int, key string) {
+func relayUDPRaw(data []byte, n int, myIP string, myPort int, key string) {
+	var myID string
+	// find my ID
 	clientUDPIPs.Range(func(robotId,ip interface{})bool{
+		// get port
 		_port, ok := clientUDPPorts.Load(robotId.(string)+":"+key)
 		if !ok {
 			fmt.Println("No key in UDP port", robotId.(string)+":"+key)
 			return true
 		}
 		port := _port.(int)
-		if port == myPort {
+		// running on the same machine may have the same IP address
+		// running on different machines may have the same port
+		if myIP == ip.(string) && myPort == port{
+			myID = robotId.(string)
+		}
+		return true
+	})
+	// recv cnt ++
+	cnt, _ := UDPRecvCnt.Load(myID+":"+key)
+	UDPRecvCnt.Store(myID+":"+key, cnt.(int)+1)
+
+	clientUDPIPs.Range(func(robotId,ip interface{})bool{
+		// get port
+		_port, ok := clientUDPPorts.Load(robotId.(string)+":"+key)
+		if !ok {
+			fmt.Println("No key in UDP port", robotId.(string)+":"+key)
+			return true
+		}
+		port := _port.(int)
+		if myIP == ip.(string) && port == myPort {
 			return true
 		}
 		clientAddr := &net.UDPAddr{IP: net.ParseIP(ip.(string)), Port: port}
@@ -105,50 +160,24 @@ func relayUDPRaw(data []byte, n int, myPort int, key string) {
 			return true
 		}
 		socket.(*net.UDPConn).WriteToUDP(data[:n], clientAddr)
+		// relay cnt ++
+		cnt, _ := UDPRelayCnt.Load(myID+":"+key)
+		UDPRelayCnt.Store(myID+":"+key, cnt.(int)+1)
 		return true
 	})
 }
 
-func checkErr(err error){
-	if err != nil {
-		fmt.Println(err)
+func readTCP(socket *net.TCPListener, key string){
+	for {
+		conn, err := socket.Accept()
+		fmt.Printf("Register TCP <%s> %s\n",conn.RemoteAddr().String(), key)
+		checkErr(err)
+		remoteAddr := conn.RemoteAddr()
+		ip, _port, err := net.SplitHostPort(remoteAddr.String())
+		port,err := strconv.Atoi(_port)
+		checkErr(err)
+		go handleTCP(conn, ip, port, remoteAddr, key)
 	}
-}
-
-func relayTCP(data []byte, n int, robotID string, key string) {
-	clientTCPIPs.Range(func(otherID,value interface{})bool{
-		if otherID == robotID {
-			return true
-		}
-		conn, ok := clientTCPSockets.Load(otherID.(string)+":"+key)
-		if !ok {
-			return true
-		}
-		conn.(net.Conn).Write(data[:n])
-		return true
-	})
-}
-
-func relayTCPRaw(data []byte, n int, remoteAddr net.Addr, key string) {
-	clientTCPIPs.Range(func(robotID,value interface{})bool{
-		if _, ok := clientTCPPorts.Load(robotID.(string)+":"+key); ok {
-			port, ok := clientTCPPorts.Load(robotID.(string)+":"+key)
-			if !ok {
-				fmt.Println("No key", robotID.(string)+":"+key, " in clientTCPPorts")
-				return true
-			}
-			address := value.(string)+":"+strconv.Itoa(port.(int))
-			if address == remoteAddr.String() {
-				return true
-			}
-			conn, ok := clientTCPSockets.Load(address)
-			if !ok {
-				return true
-			}
-			conn.(net.Conn).Write(data[:n])
-		}
-		return true
-	})
 }
 
 func handleTCP(conn net.Conn, ip string, port int, remoteAddr net.Addr, key string){
@@ -180,25 +209,133 @@ func handleTCP(conn net.Conn, ip string, port int, remoteAddr net.Addr, key stri
 			checkErr(err)
 			_, err = conn.Write(feedbackStr)
 			checkErr(err)
+
+			SocketState := SocketState{Key:key, Type:"TCP", IP: ip, Port: port, RecvFPS:0, RelayFPS:0}
+			TCPStateMap.Store(remoteAddr.String(), SocketState)
 			clientTCPSockets.Store(remoteAddr.String(), conn)
+			TCPRecvCnt.Store(remoteAddr.String(), 0)
+			TCPRelayCnt.Store(remoteAddr.String(), 0)
 		} else {
-			go relayTCP(data, n, message.Id, key)
+			go relayTCP(data, n, message.Id, key, ip, port)
 		}
 	}
 	fmt.Printf("Close TCP connection <%s:%d> as %s\n", ip, port, key)
 }
 
-func readTCP(socket *net.TCPListener, key string){
+func relayTCP(data []byte, n int, robotID string, key string, ip string, port int) {
+	cnt, _ := TCPRecvCnt.Load(ip+":"+strconv.Itoa(port))
+	TCPRecvCnt.Store(ip+":"+strconv.Itoa(port), cnt.(int)+1)
+	clientTCPIPs.Range(func(otherID,value interface{})bool{
+		if otherID == robotID {
+			return true
+		}
+		conn, ok := clientTCPSockets.Load(otherID.(string)+":"+key)
+		if !ok {
+			return true
+		}
+		conn.(net.Conn).Write(data[:n])
+		cnt, _ := TCPRelayCnt.Load(ip+":"+strconv.Itoa(port))
+		TCPRelayCnt.Store(ip+":"+strconv.Itoa(port), cnt.(int)+1)
+		return true
+	})
+}
+
+func relayTCPRaw(data []byte, n int, remoteAddr net.Addr, key string) {
+	cnt, _ := TCPRecvCnt.Load(remoteAddr.String())
+	TCPRecvCnt.Store(remoteAddr.String(), cnt.(int)+1)
+	clientTCPIPs.Range(func(robotID,value interface{})bool{
+		if _, ok := clientTCPPorts.Load(robotID.(string)+":"+key); ok {
+			port, ok := clientTCPPorts.Load(robotID.(string)+":"+key)
+			if !ok {
+				fmt.Println("No key", robotID.(string)+":"+key, " in clientTCPPorts")
+				return true
+			}
+			address := value.(string)+":"+strconv.Itoa(port.(int))
+			if address == remoteAddr.String() {
+				return true
+			}
+			conn, ok := clientTCPSockets.Load(address)
+			if !ok {
+				return true
+			}
+			conn.(net.Conn).Write(data[:n])
+			cnt, _ := TCPRelayCnt.Load(remoteAddr.String())
+			TCPRelayCnt.Store(remoteAddr.String(), cnt.(int)+1)
+		}
+		return true
+	})
+}
+
+func FPSCounter() {
+	duration := time.Duration(time.Second)
+	t := time.NewTicker(duration)
+	defer t.Stop()
 	for {
-		conn, err := socket.Accept()
-		fmt.Printf("Register TCP <%s> %s\n",conn.RemoteAddr().String(), key)
-		checkErr(err)
-		remoteAddr := conn.RemoteAddr()
-		ip, _port, err := net.SplitHostPort(remoteAddr.String())
-		port,err := strconv.Atoi(_port)
-		checkErr(err)
-		go handleTCP(conn, ip, port, remoteAddr, key)
+		<- t.C
+		var states []SocketState
+		clientUDPPorts.Range(func(key,socket interface{})bool{
+			recvCnt, ok := UDPRecvCnt.Load(key.(string))
+			if !ok {
+				fmt.Println("No key in UDP socket", key.(string))
+				return true
+			}
+			relayCnt, ok := UDPRelayCnt.Load(key.(string))
+			if !ok {
+				fmt.Println("No key in UDP socket", key.(string))
+				return true
+			}
+			_state, ok := UDPStateMap.Load(key.(string))
+			state := _state.(SocketState)
+			state.RecvFPS = recvCnt.(int)
+			state.RelayFPS = relayCnt.(int)
+			states = append(states, state)
+			newState := SocketState{Key:state.Key, Type:"UDP", IP: state.IP, Port: state.Port, RecvFPS:0, RelayFPS:0}
+			UDPStateMap.Store(key.(string), newState)
+			UDPRecvCnt.Store(key.(string), 0)
+			UDPRelayCnt.Store(key.(string), 0)
+			return true
+		})
+		clientTCPSockets.Range(func(key,socket interface{})bool{
+			recvCnt, ok := TCPRecvCnt.Load(key.(string))
+			if !ok {
+				fmt.Println("No key in TCP socket", key.(string))
+				return true
+			}
+			relayCnt, ok := TCPRelayCnt.Load(key.(string))
+			if !ok {
+				fmt.Println("No key in TCP socket", key.(string))
+				return true
+			}
+			_state, ok := TCPStateMap.Load(key.(string))
+			state := _state.(SocketState)
+			state.RecvFPS = recvCnt.(int)
+			state.RelayFPS = relayCnt.(int)
+			states = append(states, state)
+			newState := SocketState{Key:state.Key, Type:"TCP", IP: state.IP, Port: state.Port, RecvFPS:0, RelayFPS:0}
+			TCPStateMap.Store(key.(string), newState)
+			TCPRecvCnt.Store(key.(string), 0)
+			TCPRelayCnt.Store(key.(string), 0)
+			return true
+		})
+		mutex.Lock()
+		socketStates = states
+		mutex.Unlock()
 	}
+}
+
+func initTemplate(fileName string) (err error) {
+	myTemplate, err = template.ParseFiles(fileName)
+	checkErr(err)
+	return err
+}
+
+func webHandler(writer http.ResponseWriter, request *http.Request) {
+	data := make(map[string]interface{})
+	data["title"] = "Data Relay"
+	mutex.RLock()
+	data["states"] = socketStates
+	mutex.RUnlock()
+	myTemplate.Execute(writer, data)
 }
 
 func main() {
@@ -207,7 +344,7 @@ func main() {
 		clientListener, err := net.ListenUDP("udp", clientAddr)
 		checkErr(err)
 		clientUDPSockets.Store(key, clientListener)
-		go read(clientListener, key)
+		go readUDP(clientListener, key)
 	}
 
 	for key, port := range portsDictTCP {
@@ -217,5 +354,10 @@ func main() {
 		checkErr(err)
 		go readTCP(clientListener, key)
 	}
+	go FPSCounter()
+	initTemplate("./index.html")
+	http.HandleFunc("/", webHandler)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	checkErr(err)
 	<-done
 }
